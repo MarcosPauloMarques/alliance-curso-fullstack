776fec3137205533cfa15da22a62fabfc7b746b4
Marcos Paulo <marcospaulomarquesdelima@yahoo.com.br>
2019-12-18 17:37:52 -0300

    Trabalho dia3

---
+++ dia3/backend/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+
+import Usuarios from './usuarios';
+import Bicicletas from './bicicletas';
+
+    const app = express();
+    
+    app.use(cors());
+    app.use(express.json());
+
+    app.use('/usuarios', Usuarios);
+    app.use('/bicicletas', Bicicletas);
+
+    app.listen(3000, () =>
+        console.log('Servidor rodando na porta 3000!'),
+    );
+
