d13cda2b48a7cd516596c9bbcf1cc12803c40c61
Marcos Paulo <marcospaulomarquesdelima@yahoo.com.br>
2019-12-19 17:52:27 -0300

    Trabalho dia4

---
+++ dia3/frontend/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/Server.png">
+    <p>
+    </p>
+    <p>
+      Pagina com informações sobre diversas tabelas criadas em PostqreSQL
+    </p>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+ 
+}
+</script>
